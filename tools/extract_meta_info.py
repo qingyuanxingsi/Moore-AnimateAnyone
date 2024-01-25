506413c7c7ed9743890714fee299970bf7eb45fb
doodleliang <doodleliang@tencent.com>
2024-01-26 00:10:20 +0800

    feat(ci): update meta

--- tools/extract_meta_info.py
+++ tools/extract_meta_info.py
@@ -9,16 +9,16 @@ import os
 # python tools/extract_meta_info.py --root_path /path/to/video_dir --dataset_name fashion
 # -----
 parser = argparse.ArgumentParser()
-parser.add_argument("--root_path", type=str)
 parser.add_argument("--dataset", type=str)
 
 args = parser.parse_args()
 
-pose_dir = args.root_path + "_dwpose"
+root_path = f"datasets/{args.dataset}/seg_filter"
+pose_dir = f"datasets/{args.dataset}/seg_dwpose"
 
 # collect all video_folder paths
 video_mp4_paths = set()
-for root, dirs, files in os.walk(args.root_path):
+for root, dirs, files in os.walk(root_path):
     for name in files:
         if name.endswith(".mp4"):
             video_mp4_paths.add(os.path.join(root, name))
@@ -26,7 +26,7 @@ video_mp4_paths = list(video_mp4_paths)
 
 meta_infos = []
 for video_mp4_path in video_mp4_paths:
-    relative_video_name = os.path.relpath(video_mp4_path, args.root_path)
+    relative_video_name = os.path.relpath(video_mp4_path, root_path)
     kps_path = os.path.join(pose_dir, relative_video_name)
     meta_infos.append({"video_path": video_mp4_path, "kps_path": kps_path})
 
